ee51aee4bdd66a8def19cf700a0202e9aa0dd07d
Pedro Javier <pjpc@ic.ufal.br>
2021-03-17 23:25:44 -0300

    feat: add uuid to categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,15 @@
 import { Router, Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request: Request, response: Response) => {
+categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
   categories.push({
+    id: uuidv4(),
     name,
     description,
   });
