7cf6ba6b9a25d0a5a74e167eb8173af18de58d43
Pedro Javier <pjpc@ic.ufal.br>
2021-03-19 12:19:52 -0300

    feat: read upload file data

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,10 @@
+import { CategoriesRepository } from '../../repositories/implementations/categories.repository';
 import { ImportCategoryController } from './importCategory.controller';
 import { ImportCategoryUseCase } from './importCategory.useCase';
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepository.getInstance();
+
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 
 const importCategoryController = new ImportCategoryController(
   importCategoryUseCase,
