1c319362241d482543673629f2ced4a14c5242fe
Pedro Javier <pjpc@ic.ufal.br>
2021-03-24 20:47:57 -0300

    feat: create users repository and createUserUseCase

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/users.repository';
+import { IUsersRepository } from '../../modules/accounts/repositories/interfaces/IUsers.repository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/categories.repository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/specifications.repository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/interfaces/ICategories.repository';
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   'SpecificationsRepository',
   SpecificationsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
 );
