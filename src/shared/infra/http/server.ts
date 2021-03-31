f965c824b31e9ea97419c2987a1c726777c58c0d
Pedro Javier <pjpc@ic.ufal.br>
2021-03-31 10:17:43 -0300

    feat: add server and typeorm config to git

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import swaggerUI from 'swagger-ui-express';
+
+import '../typeorm';
+import '../../container';
+
+import { AppError } from 'shared/errors/app.error';
+
+import swaggerFile from '../../../swagger.json';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    console.log(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+app.listen(3333, () => console.log('Server is running at port :3333'));
