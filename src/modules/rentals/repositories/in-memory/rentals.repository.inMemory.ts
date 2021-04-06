b1023e53fa0ecd620a8d69f371c3e820d7c96603
Pedro Javier <pjpc@ic.ufal.br>
2021-04-06 09:57:42 -0300

    feat: work with dates

--- src/modules/rentals/repositories/in-memory/rentals.repository.inMemory.ts
+++ src/modules/rentals/repositories/in-memory/rentals.repository.inMemory.ts
@@ -1,18 +1,32 @@
+import { CreateRentalDto } from 'modules/rentals/dtos/createRental.dto';
 import { Rental } from 'modules/rentals/infra/typeorm/entities/rental.entity';
 import { IRentalsRepository } from 'modules/rentals/repositories/interfaces/IRentals.repository';
 
 class RentalsRepositoryInMemory implements IRentalsRepository {
   rentals: Rental[] = [];
 
+  async create(data: CreateRentalDto): Promise<Rental> {
+    const rental = new Rental();
+
+    Object.assign(rental, {
+      ...data,
+      start_date: new Date(),
+    });
+
+    this.rentals.push(rental);
+
+    return rental;
+  }
+
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
     return this.rentals.find(
-      rental => rental.car_id === car_id && rental.end_date === null,
+      rental => rental.car_id === car_id && !rental.end_date,
     );
   }
 
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
     return this.rentals.find(
-      rental => rental.user_id === user_id && rental.end_date === null,
+      rental => rental.user_id === user_id && !rental.end_date,
     );
   }
 }
--- src/modules/rentals/repositories/interfaces/IRentals.repository.ts
+++ src/modules/rentals/repositories/interfaces/IRentals.repository.ts
@@ -1,6 +1,8 @@
+import { CreateRentalDto } from 'modules/rentals/dtos/createRental.dto';
 import { Rental } from 'modules/rentals/infra/typeorm/entities/rental.entity';
 
 interface IRentalsRepository {
+  create(data: CreateRentalDto): Promise<Rental>;
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
 }
